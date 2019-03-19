d8de28fe01941f9cee77438105cd18b1a8fb2bb4
Shumbaneverdies <tapiwataps13@gmail.com>
2019-03-19 19:34:04 +0200

    new changes uploaded

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -5,18 +5,14 @@ type StartingPosition =
     | North
 
     
-type House = {
-    postion: StartingPosition
-    locationnum: int
-    seedCount: int
-}
+
 type Store = {
     capacity: int
     player: StartingPosition
 }
 type Board = {
     position: StartingPosition
-    houses: House*House*House*House*House*House*House*House*House*House*House*House
+    houses: (int*int*int*int*int*int*int*int*int*int*int*int)
     stores: Store*Store
 }
 
@@ -24,8 +20,12 @@ let getSeeds n board = failwith "Not implemented"
 
 let useHouse n board = failwith "Not implemented"
 
-let start position = failwith "Not implemented"
-
+let start p = 
+        let k = {
+            position=South
+        }
+             
+      
 let score board = failwith "Not implemented"
 
 let gameState board = failwith "Not implemented"
