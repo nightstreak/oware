ee77b6254e61889bb4ed70fd5d8fff4e2c17773e
Shumbaneverdies <tapiwataps13@gmail.com>
2019-03-19 19:49:15 +0200

    finalised data structure

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -1,29 +1,31 @@
 ﻿module Oware
 
-type StartingPosition =
+type StartingPosition =   //players are named after their startingpostition 
     | South
     | North
 
     
 
-type Store = {
+type Store = {  //player
     capacity: int
     player: StartingPosition
+    atCapacity : bool //I win (25)
 }
 type Board = {
     position: StartingPosition
-    houses: (int*int*int*int*int*int*int*int*int*int*int*int)
-    stores: Store*Store
+    houses: (int*int*int*int*int*int*int*int*int*int*int*int) //first six belong to south 
+    stores: Store*Store    ///puting players on the board
 }
 
 let getSeeds n board = failwith "Not implemented"
 
 let useHouse n board = failwith "Not implemented"
 
-let start p = 
-        let k = {
-            position=South
-        }
+let start p = "fail"
+
+       
+                        
+        
              
       
 let score board = failwith "Not implemented"
