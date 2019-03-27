0e90e3d0c3c297b8e328467fbcf6e2fbd43428e9
nighstreak <G17M4529@ict.ru.ac.za>
2019-03-27 18:47:51 +0200

    small fixes

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -22,16 +22,12 @@ type GameState =
 
 type Board = {
     position: StartingPosition
-<<<<<<< HEAD
     stores: Store*Store    ///puting players on the board   (first store belongs to south)
-=======
-    stores: Store*Store
->>>>>>> Thapelo'sBranch
     gameState: GameState
 }
 
 
-<<<<<<< HEAD
+
 let getSeeds n board = //houses are numbered 1 to 12 with the first 6 being the houses in South (first store belongs to south)
     match n with 
     |1 -> match board with //structuly decomposing board
@@ -62,24 +58,18 @@ let getSeeds n board = //houses are numbered 1 to 12 with the first 6 being the 
           |{position=_;stores=(a,b);gameState=_} -> match b with 
                                                     |{capacity=_;player=_;smallHouse=(q,w,e,r,t,y)} -> e
     |10 -> match board with 
-          |{position=_;stores=(a,b);gameState=_} -> match b with 
+           |{position=_;stores=(a,b);gameState=_} -> match b with 
                                                     |{capacity=_;player=_;smallHouse=(q,w,e,r,t,y)} -> r
     |11 -> match board with 
-          |{position=_;stores=(a,b);gameState=_} -> match b with 
+           |{position=_;stores=(a,b);gameState=_} -> match b with 
                                                     |{capacity=_;player=_;smallHouse=(q,w,e,r,t,y)} -> t
     |12 -> match board with 
-          |{position=_;stores=(a,b);gameState=_} -> match b with 
+           |{position=_;stores=(a,b);gameState=_} -> match b with 
                                                     |{capacity=_;player=_;smallHouse=(q,w,e,r,t,y)} -> y // i know there is a better way to do this but im to lazy to do the typing
     |_ -> failwith "invalid house number"
 
-=======
-<<<<<<< HEAD
-let getSeeds n board = failwith "Not implemented"
-=======
 
-let getSeeds n board = failwith "Not implement3ed"
->>>>>>> 2c18751de7bcde665234ec8e6e868d493530a3c2
->>>>>>> Thapelo'sBranch
+
 
 let useHouse n board = failwith "Not implemented"
 
@@ -101,11 +91,11 @@ let score board =
 
 let gameState board = 
     match board.gameState with
-    |South'sTurn -> "South's Turn"
-    |North'sTurn -> "North's Turn"
+    |South'sTurn -> "South's turn"
+    |North'sTurn -> "North's turn"
     |Draw -> "Draw"
-    |SouthWin -> "South Won"
-    |NorthWin -> "North Won"
+    |SouthWin -> "South won"
+    |NorthWin -> "North won"
 
 [<EntryPoint>]
 let main _ =
